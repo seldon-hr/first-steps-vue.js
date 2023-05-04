2da0eaf9e242a6dd6edadd083fb0d5c980a0aedb
seldon-hr <daanmaro99june@gmail.com>
2023-05-04 15:31:48 -0600

    Use data, methods, and template of a component at vue

--- buttons/buttons.js
+++ buttons/buttons.js
@@ -2,18 +2,18 @@ const vm = Vue.createApp({
     //.createApp means Sea un JSON que describe nuestra applicación, conocida como optionAPI
     data(){
         return {
-            text: "<h2>A heading two</h2>"
+            counter: 0
         };
     },
-    template: `<div v-once v-html="text"></div>`  //Escribiendo directives dentro del vue
-            //v-text estamos crendo una directive que nos llama a la variable que tenemos.
-}).mount('#app');
+    methods: {
+        increment() {
+            this.counter++;
+            /* Uso de this, para poder hacer uso de los misma data y llamar a los
+            components */
+        }
+    },
+    template: `<button v-on:click="increment">{{ counter }}</button>`  
+    /* Usando v-on, ya no es necesario llamar a una opción que escuché el dom,
+    sino ya solo lo está haciendo. */
+}).mount("#app");
 
-console.log(vm);
-/*El retorno que generá el  vm, es un proxy y este puede ser modificado en 
-JS vainilla, es decir se modifica con los datos.  
-
-VM, objeto que contiene toda nuestra aplicación
-ViewModel como modelo que nos pemirte organizar, estructurar aquellos bloques 
-que queremos que se vean, así como su segmentación.
-*/
