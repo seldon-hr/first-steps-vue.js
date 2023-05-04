83dbe5df94f446b2099dbc3b0507708775ad9443
seldon-hr <daanmaro99june@gmail.com>
2023-05-04 16:02:42 -0600

    Reacitividad de los componentes desde vue

--- buttons/buttons.js
+++ buttons/buttons.js
@@ -1,8 +1,10 @@
 const vm = Vue.createApp({
     //.createApp means Sea un JSON que describe nuestra applicación, conocida como optionAPI
     data(){
-        return {
-            counter: 0
+        /* En la data, va ir todas aquellas variables o data que se maneja en el
+        en el componente. */
+        return {  
+            counter: 0   
         };
     },
     methods: {
@@ -10,10 +12,27 @@ const vm = Vue.createApp({
             this.counter++;
             /* Uso de this, para poder hacer uso de los misma data y llamar a los
             components */
+        },
+
+        decrement() {
+            this. counter--;
         }
+
     },
-    template: `<button v-on:click="increment">{{ counter }}</button>`  
+    template: `
+    <button v-on:click="increment">
+        +1
+    </button>
+    <button v-on:click="decrement">
+        -1
+    </button>
+    <span>
+        {{ counter }}
+    </span>
+    `  
     /* Usando v-on, ya no es necesario llamar a una opción que escuché el dom,
-    sino ya solo lo está haciendo. */
+    sino ya solo lo está haciendo. 
+    
+    Poder emplear métodos de JS*/
 }).mount("#app");
 
