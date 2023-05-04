c57485270bdf4769a927bc9b4e470f9cc9dc17ac
seldon-hr <daanmaro99june@gmail.com>
2023-05-03 18:04:26 -0600

    Use of directives on vue app

--- buttons/buttons.js
+++ buttons/buttons.js
@@ -2,9 +2,18 @@ const vm = Vue.createApp({
     //.createApp means Sea un JSON que describe nuestra applicación, conocida como optionAPI
     data(){
         return {
-            text: "new proof"
+            text: "<h2>A heading two</h2>"
         };
-    }
+    },
+    template: `<div v-once v-html="text"></div>`  //Escribiendo directives dentro del vue
+            //v-text estamos crendo una directive que nos llama a la variable que tenemos.
 }).mount('#app');
 
-console.log(vm)
+console.log(vm);
+/*El retorno que generá el  vm, es un proxy y este puede ser modificado en 
+JS vainilla, es decir se modifica con los datos.  
+
+VM, objeto que contiene toda nuestra aplicación
+ViewModel como modelo que nos pemirte organizar, estructurar aquellos bloques 
+que queremos que se vean, así como su segmentación.
+*/
