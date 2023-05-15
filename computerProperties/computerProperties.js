2afc9f953f149ede9b357de8c594c35e53f1caaa
seldon-hr <daanmaro99june@gmail.com>
2023-05-15 17:26:50 -0600

    Basic use of computer properties.

--- computerProperties/computerProperties.js
+++ computerProperties/computerProperties.js
@@ -20,3 +20,26 @@ const vm = Vue.createApp({
         <div> {{ today }}</div>
     `,
 }).mount("#app");
+
+/* La diferencia entre hacer la declaración de de la Vue app y hacer el 
+el mount al final del bloque de código.
+e.g. 
+
+const vm = Vue.createApp({
+    ...
+}).mount("#app");
+
+En este caso se guarda una referencia a la instancia de la app de Vue en 
+una variable, lo que permite acceder a ella en cualquier momento  y manipular
+su estado o comportamiento después de haber sido creada.
+
+Por el otro lado, si se realizaá el mount, fuera del bloque de la creación de la app,
+no se guardará una referencia a la instancia de la app.
+
+const vm = Vue.createApp({
+    ...
+});
+
+vm.mount("#app");
+
+*/
