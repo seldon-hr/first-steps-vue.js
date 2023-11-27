4a4d2c9e5e68e9502055756d9406e02888fdfb14
Daniel Romero <daanmaro99june@gmail.com>
2023-11-26 22:07:54 -0600

    feat: "Using intellicode "

---
+++ intellicode/main.js
@@ -0,0 +1,15 @@
+const userName = "Leto";
+const userLastName = "Atredis";
+const userFullName = userName.concat("", userLastName);
+const userEmail = "leto@atredis.com";
+const currentDate = new Date().toUTCString
+/* Con la bombilla nos permite ver proyecto con el nombre de esa función sobre
+otros proyecto, y en que repositorios se esta basando. */
+
+const printValue (userName, userLastName, userFullName, currentDate) => {
+    console.log(userName);
+    console.log(userLastName);
+    console.log(userFullName);
+    console.log(currentDate);
+}
+ 
